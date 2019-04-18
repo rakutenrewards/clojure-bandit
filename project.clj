03fe8fdda440252d132bade7eeb94f625f68a43e
Connor Clark <connor@curbside.com>
2019-04-18 10:08:43 -0700

    lein-release plugin: bumped version from 0.1.4 to 0.1.5-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.curbside/curbside-bandit "0.1.4"
+(defproject com.curbside/curbside-bandit "0.1.5-SNAPSHOT"
   :description "Multi-armed bandit algorithms"
   :url "https://github.com/Curbside/curbside-bandit"
   :dependencies [[org.clojure/clojure "1.10.0"]
