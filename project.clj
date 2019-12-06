3f37b83fdb423ca6a3c113e830d12e27ca3e3a06
Olivier Falardeau <olivier@curbside.com>
2019-12-06 09:41:54 -0500

    lein-release plugin: bumped version from 0.1.8 to 0.1.9-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.curbside/curbside-bandit "0.1.8"
+(defproject com.curbside/curbside-bandit "0.1.9-SNAPSHOT"
   :description "Multi-armed bandit algorithms"
   :url "https://github.com/Curbside/curbside-bandit"
   :dependencies [[org.clojure/clojure "1.10.1"]
