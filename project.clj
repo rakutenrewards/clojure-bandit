4f068da18f0e734fc1442f56d9a78ef0fa985030
Olivier Falardeau <olivier@curbside.com>
2019-12-18 15:24:21 -0500

    lein-release plugin: preparing 0.2.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.curbside/curbside-bandit "0.1.9-SNAPSHOT"
+(defproject com.curbside/curbside-bandit "0.2.0"
   :description "Multi-armed bandit algorithms"
   :url "https://github.com/Curbside/curbside-bandit"
   :dependencies [[org.clojure/clojure "1.10.1"]
