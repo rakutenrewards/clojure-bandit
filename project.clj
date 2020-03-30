5d5f484b9b2c8196f7b9c6316dadee3f6d8522fc
Connor Clark <connor.clark@rakuten.com>
2020-03-30 14:09:11 -0700

    lein-release plugin: preparing 0.2.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.curbside/curbside-bandit "0.2.2-SNAPSHOT"
+(defproject com.curbside/curbside-bandit "0.2.2"
   :description "Multi-armed bandit algorithms"
   :url "https://github.com/Curbside/curbside-bandit"
   :dependencies [[org.clojure/clojure "1.10.1"]
